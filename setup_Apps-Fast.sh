95ae1b4b2cf5fb6f81e3094965e7e38e41243ad2
Julian Fleck Development <julian.fleck.dev@gmail.com>
2026-02-27 18:52:56 +0100

    Update setup_Apps-Fast.sh

--- setup_Apps-Fast.sh
+++ setup_Apps-Fast.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt update; sudo apt upgrade -y
-
-#Remove Bloatware
-sudo apt remove warpinator
-sudo apt remove hypnotix
-
 read -p "Do you want to install VLC? (y/n) " install_VLC
 read -p "Do you want to install Gimp? (y/n) " install_Gimp
 read -p "Do you want to install Bitwarden? (y/n) " install_BW
+
+sudo apt update; sudo apt upgrade -y
+
+#Remove Bloatware
+sudo apt remove warpinator -y
+sudo apt remove hypnotix -y
 
 if [[ "$install_VLC" =~ ^[Yy]$ ]]; then
     sudo apt install vlc -y
@@ -19,7 +19,7 @@ if [[ "$install_Gimp" =~ ^[Yy]$ ]]; then
 fi
 
 if [[ "$install_BW" =~ ^[Yy]$ ]]; then
-    sudo flatpak install flathub com.bitwarden.desktop
+    sudo flatpak install flathub com.bitwarden.desktop -y
 fi
 
 echo "Finished Setup"
